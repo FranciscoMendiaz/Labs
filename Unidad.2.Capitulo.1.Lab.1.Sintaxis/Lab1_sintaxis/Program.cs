086aff1ad746c8dd9ad63cfbeb91b940d735277b
Francisco Mendiaz <franciscomendiaz24@gmail.com>
2023-04-21 18:56:41 -0300

    Resolución Laboratorio 1 ejercicio 1

--- Unidad.2.Capitulo.1.Lab.1.Sintaxis/Lab1_sintaxis/Program.cs
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Lab1_sintaxis/Program.cs
@@ -1,2 +1,84 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/* --------------- Ejercicio 1 estructuras de decisión con IF ----------
+
+string inputTexto="negros";
+do
+{
+    Console.Write("Texto: ");
+    inputTexto = Console.ReadLine();
+    Console.Clear();
+} while (inputTexto == "");
+
+
+Console.WriteLine("1) To Upper");
+Console.WriteLine("2) To lower");
+Console.WriteLine("3) Length");
+
+
+
+ConsoleKeyInfo opcion = Console.ReadKey();
+
+if (opcion.Key == ConsoleKey.D1)
+{
+    Console.Clear();
+    Console.WriteLine(inputTexto.ToUpper());
+}
+
+else if (opcion.Key == ConsoleKey.D2)
+{
+    Console.Clear();
+    Console.WriteLine(inputTexto.ToLower());
+}
+
+else if (opcion.Key == ConsoleKey.D3)
+{
+    Console.Clear();
+    int largo = inputTexto.Length;
+    Console.WriteLine(largo);
+}
+--------------------------------------------------------------------- */
+
+//  Ejercicio 1 estructuras de decisión con CASE 
+
+
+string inputTexto="negros";
+do
+{
+    Console.Write("Texto: ");
+    inputTexto = Console.ReadLine();
+    Console.Clear();
+} while (inputTexto == "");
+
+
+Console.WriteLine("1) To Upper");
+Console.WriteLine("2) To lower");
+Console.WriteLine("3) Length");
+
+
+
+ConsoleKeyInfo opcion = Console.ReadKey();
+
+Console.Clear();
+
+switch (opcion.Key)
+{
+    case ConsoleKey.D1:
+    Console.WriteLine(inputTexto.ToUpper());
+    break;
+
+    case ConsoleKey.D2:
+    Console.WriteLine(inputTexto.ToLower());
+    break;
+
+    case ConsoleKey.D3:
+    int largo = inputTexto.Length;
+    Console.WriteLine(largo);
+    break;
+
+    default: Console.WriteLine("Ingrese una opción válida");
+    break;
+}
+
+
+
