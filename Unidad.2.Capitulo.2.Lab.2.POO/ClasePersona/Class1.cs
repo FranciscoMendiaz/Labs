04324e5d1bbf9d0143cb689abc6e7016456a8b9f
Francisco Mendiaz <franciscomendiaz24@gmail.com>
2023-05-15 14:30:53 -0300

    Agrego el ejercicio clase persona

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/ClasePersona/Class1.cs
@@ -0,0 +1,76 @@
+﻿namespace ClasePersona
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private int dni;
+
+        public Persona()
+        {
+            this.nombre = "Persona";
+            this.apellido = "Sin nombre";
+            Console.WriteLine("Se ha creado el objeto persona sin nombre");
+        }
+
+        public Persona(string nom, string ape)
+        {
+            this.nombre = nom;
+            this.apellido = ape;
+            Console.WriteLine("Se ha creado el objeto persona con nombre {0} y apellido {1}", this.nombre, this.apellido);
+        }
+
+        ~Persona()
+        {
+            Console.WriteLine("Se procederá a destruir el objeto"); 
+        }
+
+        public string Apellido
+        {
+            get => this.apellido;
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get =>nombre;
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Dni
+        {
+            get => dni;
+            set
+            {
+                dni = value;
+            }
+        }
+
+        public int Edad
+        {
+            get => edad;
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public string getFullName()
+        {
+            return(nombre+apellido);
+        }
+        
+  
+
+
+
+    }
+
+}
