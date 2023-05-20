e4869c034836aed7ee031b004aca204ce2cb823b
Francisco Mendiaz <franciscomendiaz24@gmail.com>
2023-05-20 12:05:23 -0300

    Agrego todo a EJS4

--- Unidad.2.Capitulo.1.Lab.1.Sintaxis/Lab1_Sintaxis_Ejs4/Program.cs
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Lab1_Sintaxis_Ejs4/Program.cs
@@ -1,6 +1,117 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/* Ejercicio 4 (1)
+
+/* --------------- Ejercicio 1 estructuras de decisión con IF ----------
+
+string inputTexto="a";
+do
+{
+    Console.Write("Texto: ");
+    inputTexto = Console.ReadLine();
+    Console.Clear();
+} while (inputTexto == "");
+
+
+Console.WriteLine("1) To Upper");
+Console.WriteLine("2) To lower");
+Console.WriteLine("3) Length");
+
+
+
+ConsoleKeyInfo opcion = Console.ReadKey();
+
+if (opcion.Key == ConsoleKey.D1)
+{
+    Console.Clear();
+    Console.WriteLine(inputTexto.ToUpper());
+}
+
+else if (opcion.Key == ConsoleKey.D2)
+{
+    Console.Clear();
+    Console.WriteLine(inputTexto.ToLower());
+}
+
+else if (opcion.Key == ConsoleKey.D3)
+{
+    Console.Clear();
+    int largo = inputTexto.Length;
+    Console.WriteLine(largo);
+}
+
+
+--------------- Ejercicio 2 estructuras de decisión con CASE ----------
+
+
+
+string inputTexto="s";
+do
+{
+    Console.Write("Texto: ");
+    inputTexto = Console.ReadLine();
+    Console.Clear();
+} while (inputTexto == "");
+
+
+Console.WriteLine("1) To Upper");
+Console.WriteLine("2) To lower");
+Console.WriteLine("3) Length");
+
+
+
+ConsoleKeyInfo opcion = Console.ReadKey();
+
+Console.Clear();
+
+switch (opcion.Key)
+{
+    case ConsoleKey.D1:
+    Console.WriteLine(inputTexto.ToUpper());
+    break;
+
+    case ConsoleKey.D2:
+    Console.WriteLine(inputTexto.ToLower());
+    break;
+
+    case ConsoleKey.D3:
+    int largo = inputTexto.Length;
+    Console.WriteLine(largo);
+    break;
+
+    default: Console.WriteLine("Ingrese una opción válida");
+    break;
+}
+
+
+
+
+--------------- EJERCICIO 3 --------------------------------------------------
+
+int cantIteraciones = 5;
+string[] arreglo = new string[cantIteraciones];
+
+
+for (int i =0; i < cantIteraciones; i++)
+{
+    Console.Write("Ingrese la posición {0} del arreglo: ", i);
+    arreglo[i] = Console.ReadLine();
+    Console.WriteLine ("");
+}
+
+Console.Clear ();
+
+for (int i = 0; i < cantIteraciones; i++)
+{
+    Console.Write("Posición {0} del arreglo: ", i);
+    Console.WriteLine(arreglo[i]);
+    Console.WriteLine("");
+}
+
+
+
+
+--------------- Ejercicio 4 (1) --------------------------------------------------------------------------------
+
 Console.Write("Primer Numero a Sumar: ");
 float num1 = float.Parse(Console.ReadLine());
 
@@ -9,10 +120,10 @@ float num2 = float.Parse(Console.ReadLine());
 
 float suma = num1 + num2;
 
-Console.WriteLine("La suma de " + num1 + " más " + num2 + " da como resultado " + suma); */
+Console.WriteLine("La suma de " + num1 + " más " + num2 + " da como resultado " + suma);
 
 
-/* Ejercicio 4 (2)
+--------------- Ejercicio 4 (2)--------------------------------------------------------------------------------
 
 int year = 3;
 
@@ -25,11 +136,11 @@ esBiciesto(year);
 static void esBiciesto(int y)
 {
     if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) Console.WriteLine("El año " + y + " es biciesto");
-    else Console.WriteLine("El año " + y + " NO es biciesto"); */
+    else Console.WriteLine("El año " + y + " NO es biciesto"); 
 
 
 
-/* Ejercicio 4 (3)
+--------------- Ejercicio 4 (3) --------------------------------------------------------------------------------
 
 int v1 = 0;
 int v2 = 1;
@@ -50,21 +161,14 @@ for (int i = 0; i <= n; i++)
 
 }
 
-
-*/
-
-
-/* Ejercicio 4 (4)
+--------------- Ejercicio 4 (4) --------------------------------------------------------------------------------
 
 for(int i=1; i <=100;i++)
 {
     if (i % 2 == 0) { Console.WriteLine("El número " + i + " es par."); }
 }
 
-*/
-
-
-/* Ejercicio 4 (5)
+--------------- Ejercicio 4 (5) --------------------------------------------------------------------------------
 
 string month;
 
@@ -128,10 +232,10 @@ switch (month)
     default: Console.WriteLine("Mes inválido");
         break;
 
-} */
+} 
 
 
-//Ejercicio 4 (8)
+--------------- Ejercicio 4 (8) --------------------------------------------------------------------------------
 int attempts = 0;
 string password = "utnFRRO";
 string text;
@@ -156,6 +260,63 @@ string submitPassword()
     Console.Clear();
     return passAttempt;
 }
+
+--------------------------------------------------------------------------------------------------------------*/
+
+
+// Ejercicio 4(9)
+
+Console.WriteLine("Ingrese un numero de filas");
+int filas = Int32.Parse(Console.ReadLine());
+Console.WriteLine();
+int cantEstrellitas = 1, totalEstrellitas = 1 + 2 * (filas - 1);
+for (int i = 1; i <= filas; i++)
+{
+    blanco(totalEstrellitas, cantEstrellitas);
+    estrellita(cantEstrellitas);
+    blanco(totalEstrellitas, cantEstrellitas);
+    cantEstrellitas += 2;
+    Console.WriteLine();
+}
+
+
+static void estrellita(int cantEstrellitas)
+{
+    for (int j = 1; j <= cantEstrellitas; j++)
+    {
+        Console.Write("*");
+    }
+}
+
+static void blanco(int totalEstrellitas, int cantEstrellitas)
+{
+    int diferenciaEstrellitas = totalEstrellitas - cantEstrellitas;
+    for (int j = 1; j <= (diferenciaEstrellitas / 2); j++)
+    {
+        Console.Write(" ");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
