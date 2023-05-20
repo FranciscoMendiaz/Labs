b6c071a317cc64f45b7bd0be0d2673a074cf5af6
Francisco Mendiaz <franciscomendiaz24@gmail.com>
2023-05-20 18:01:40 -0300

    agrego ejercicio 2

---
+++ Unidad.2.Extra.Lab.3.LINQ/EjercicioLINQ/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/* 
+Crear un programa que acepte una lista de números, los almacene en un objeto del tipo List<int>
+y luego usando LINQ muestre por Consola aquellos que son mayores a 20 */
+
+
+
+int[] lista = { 1, 2, 3, 4, 5, 6, 90, 12, 23, 2, 4, 5, 2, 2, 4345, 53, 43, 4, 2 };
+
+List<int> unaLista = new List<int>();
+
+for (int i = 0; i < lista.Length; i++)
+{
+    unaLista.Add(lista[i]);
+}
+
+var filtrada = from a in unaLista
+               where a > 20
+               select a;
+
+foreach (var f  in filtrada)
+{
+    Console.WriteLine(i);
+    Console.WriteLine("");
+}
