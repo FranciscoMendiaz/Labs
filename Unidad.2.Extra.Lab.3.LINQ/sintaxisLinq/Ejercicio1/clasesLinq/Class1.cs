ca7d25ab9911baab20f891dfe614e92d6c9cc448
Francisco Mendiaz <franciscomendiaz24@gmail.com>
2023-05-20 17:16:35 -0300

    Agrego ejercicio 1 de LINQ y modifico carpetas personales

---
+++ Unidad.2.Extra.Lab.3.LINQ/sintaxisLinq/Ejercicio1/clasesLinq/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace clasesLinq
+{
+    public class Provincia
+    {
+        private string nombre;
+
+        public Provincia()
+        {
+           nombre = string.Empty;
+        }
+
+        public Provincia(string unName)
+        {
+            nombre = unName;
+        }
+
+        public string Nombre
+        {
+            get =>  nombre;
+            set {nombre = value;}
+        }
+    }
+}
---
+++ Unidad.2.Extra.Lab.3.LINQ/sintaxisLinq/Ejercicio1/programa/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using clasesLinq;
+
+
+/* Ejercicio Usando Clase Provincia
+
+Provincia[] provincias = new Provincia[5];
+
+for (int i = 0; i < provincias.Length; i++)
+{
+    Console.Write("Provincia Numero {0}: ", i+1);
+    string n = Console.ReadLine();
+    Provincia unaProv = new Provincia(n);
+    provincias[i] = unaProv;
+} // Realmente no se me ocurre otra forma de poder tener un arreglo con las provincias y usar linq. Por Ahi no es muy eficiente cargar todo.
+
+Console.Clear();
+
+var misProv = from prov in provincias
+              where prov.Nombre.StartsWith("T")|prov.Nombre.StartsWith("S")
+              select prov;
+             
+*/
+
+
+// Ejercicio usando el arreglo normal
+
+string[] provincias = { "Santa Fe", "San Juan", "La Pampa", "Tucuman", "Buenos Aires", "Mendoza", "Catamarca", "Corrientes", "Entre Rios",
+                        "San Luis", "Cordoba", "La Rioja", "Tierra del Fuego", "Chubut", "Salta", "Rio Negro", "Neuquen", "Formosa",
+                        "Misiones", "Chaco", "Jujuy", "Santa Cruz", "Santiago del Estero"};
+
+var misProv = from prov in provincias
+              where prov.StartsWith("T") | prov.StartsWith("S")
+              select prov;
+
+foreach (var prov in misProv)
+{
+    Console.WriteLine(prov);
+    Console.WriteLine("");
+}
+
+
+
