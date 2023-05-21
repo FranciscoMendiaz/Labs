e4310460e0f60df2294d22d7c8c2f123d3c70804
Francisco Mendiaz <franciscomendiaz24@gmail.com>
2023-05-20 21:06:33 -0300

    Agrego Ejercicio 4

---
+++ Unidad.2.Extra.Lab.3.LINQ/EjercicioLINQ/Ejercicio4/ClasesEj4LINQ/Empleado.cs
@@ -0,0 +1,48 @@
+﻿namespace ClasesEj4LINQ
+{
+    public class Empleado
+    {
+        private int id;
+        private string nombre;
+        private decimal sueldo;
+
+        public Empleado()
+        {
+            nombre = string.Empty;
+        }
+
+        public Empleado(string name, int cod, decimal salary)
+        {
+            nombre = name;
+            id = cod;
+            sueldo = salary;
+        }
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public decimal Sueldo
+        {
+            get => sueldo;
+            set
+            {
+                sueldo = value;
+            }
+        }
+    }
+}
---
+++ Unidad.2.Extra.Lab.3.LINQ/EjercicioLINQ/Ejercicio4/Programa/Program.cs
@@ -0,0 +1,71 @@
+﻿using ClasesEj4LINQ;
+using System.Xml.Linq;
+
+
+/*
+Dada una List<Empleado> donde Empleado tiene las propiedades Id (int), Nombre(string),
+Sueldo(decimal).Crear un programa que acepte dar de alta Empleados en esta lista y luego
+muestre por Consola esta misma Lista ordenada por la propiedad Sueldo, tanto de manera
+ascendente como descendente.
+*/
+
+List<Empleado> empleados = new List<Empleado>();
+int opc = 1;
+
+do
+{
+
+    Console.Write("Nombre: ");
+    string n = Console.ReadLine();
+    Console.Write("Id: ");
+    int cod = Int32.Parse(Console.ReadLine());
+    Console.Write("Sueldo: ");
+    decimal s = Decimal.Parse(Console.ReadLine());
+    Empleado empleado = new Empleado(n,cod,s);
+    empleados.Add(empleado);
+    Console.Clear();
+    opc = bandera();
+
+} while (opc != 0);
+
+
+int bandera()
+{
+    int op=1;
+    
+    do {
+        Console.WriteLine("Continuar con 1 o SALIR CON 0");
+        op = Int32.Parse(Console.ReadLine());
+        }while ((op  != 0) && (op !=1));
+    
+    Console.Clear();
+    return op;
+}
+
+var filtroMayMen = from e in empleados
+             orderby e.Sueldo descending
+             select e;
+
+var filtroMenMay = from emp in empleados
+                       orderby emp.Sueldo ascending
+                       select emp;
+
+
+Console.Clear();
+Console.WriteLine("Lista ordenada de menor a mayor por sueldo");
+Console.WriteLine("");
+
+foreach (var emp in filtroMenMay)
+{
+    Console.WriteLine("Nombre: {0} ; Id: {1} ; Sueldo: {2}",emp.Nombre,emp.Id,emp.Sueldo) ;
+    
+}
+Console.WriteLine("");
+Console.WriteLine("Lista ordenada de mayor a menor por sueldo");
+Console.WriteLine("");
+
+foreach (var emp in filtroMayMen)
+{
+    Console.WriteLine("Nombre: {0} ; Id: {1} ; Sueldo: {2}", emp.Nombre, emp.Id, emp.Sueldo);
+
+}
